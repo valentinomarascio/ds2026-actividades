228b2550c6f1c257b4361a91ce030ed855d4cffd
Valentino Marascio <valentinomarascioo@gmail.com>
2026-04-15 22:19:07 -0300

    ej1: variables y operaciones

---
+++ ejercicios/c03-javascript/ej1-variables/app.js
@@ -0,0 +1,12 @@
+const nombre = "Valentino Marascio";
+const edad = 20;
+const materia = "Desarrollo de software";
+
+console.log(`Me llamo ${nombre}, tengo ${edad} anios y estoy cursando ${materia}`);
+
+let contador = 0;
+contador++;
+contador++;
+contador++;
+console.log(`valor final del contador: ${contador}`);
+
