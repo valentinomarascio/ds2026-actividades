d4047b8a82170f805b7514c8367678f024413c7e
Valentino Marascio <valentinomarascioo@gmail.com>
2026-06-04 23:17:53 -0300

    feat: routing con paginas

--- ejercicios/c09-libreria-react-layout/src/components/Layout/Header.tsx
+++ ejercicios/c09-libreria-react-layout/src/components/Layout/Header.tsx
@@ -1,16 +1,16 @@
 import { Navbar, Nav, Container } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 function Header() {
   return (
     <Navbar style={{ backgroundColor: '#0a1628' }} expand="lg">
       <Container>
-        <Navbar.Brand href="#" style={{ color: '#fff', fontWeight: 700 }}>
+        <Navbar.Brand as={Link} to="/" style={{ color: '#fff', fontWeight: 700 }}>
           📚 Libreria VM
         </Navbar.Brand>
         <Nav className="ms-auto">
-          <Nav.Link href="#" style={{ color: '#94a3b8' }}>Inicio</Nav.Link>
-          <Nav.Link href="#" style={{ color: '#94a3b8' }}>Catálogo</Nav.Link>
-          <Nav.Link href="#" style={{ color: '#94a3b8' }}>Contacto</Nav.Link>
+          <Nav.Link as={Link} to="/" style={{ color: '#94a3b8' }}>Inicio</Nav.Link>
+          <Nav.Link as={Link} to="/catalogo" style={{ color: '#94a3b8' }}>Catálogo</Nav.Link>
         </Nav>
       </Container>
     </Navbar>
--- ejercicios/c09-libreria-react-layout/src/main.tsx
+++ ejercicios/c09-libreria-react-layout/src/main.tsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import 'bootstrap-icons/font/bootstrap-icons.css'  
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 )
